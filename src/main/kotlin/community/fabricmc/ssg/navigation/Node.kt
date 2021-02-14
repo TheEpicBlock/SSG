0573254118ed3578fc3ad21e1e7e2fbd1b86eeea
Gareth Coles <gdude2002@gmail.com>
2021-02-14 10:42:40 +0000

    Require icons in navigation nodes

--- src/main/kotlin/community/fabricmc/ssg/navigation/Node.kt
+++ src/main/kotlin/community/fabricmc/ssg/navigation/Node.kt
@@ -6,10 +6,11 @@ import kotlinx.serialization.Serializable
 public data class Node(
     val path: String,
     val title: String,
+    val icon: String,
 
     val children: List<Node> = listOf(),
     val description: String? = null
 ) {
     override fun toString(): String =
-        "Node(path = \"$path\", title = \"$title\", children = [${children.joinToString(", ")}])"
+        "Node(path = \"$path\", title = \"$title\", icon = \"$icon\", children = [${children.joinToString(", ")}])"
 }
